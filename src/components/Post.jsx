0b9ba5179db5641e9351ac214ecc1d07fe33872b
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-10-09 21:14:59 -0300

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,7 @@
 import styles from "./Post.module.css";
 export function Post() {
   return (
-    <div className={styles.post}>
+    <article className={styles.post}>
       <header className={styles.header}>
         <div className={styles.profile}>
           <img src="https://github.com/tiagoaraujodev.png" alt="User Picture" />
@@ -10,8 +10,23 @@ export function Post() {
             <span>Web developer</span>
           </div>
         </div>
-        <div className={styles.postStatus}>Publicado há 1h</div>
+        <time
+          dateTime="2022-05-11 08:13:30"
+          title="11 de novembro às 08:13"
+          className={styles.postStatus}
+        >
+          Publicado há 1h
+        </time>
       </header>
-    </div>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>👉<a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
   );
 }
